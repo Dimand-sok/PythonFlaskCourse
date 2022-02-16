f472950933a19dd394d3bdd272f2c93f87739dd8
Dimand-Sok <dimand.sok@gmail.com>
2022-02-16 15:42:54 +0700

    updated

--- app/app.py
+++ app/app.py
@@ -1,16 +1,18 @@
+from flask import Flask, Blueprint, render_template
 
-from flask import Flask, Blueprint, current_app
-
-#local import
-from app.routes import * 
+from app.routes import *
 
 app = Flask(__name__)
 
-#register in app if instance is Blueprint
 all_routes = [route for name, route in globals().items() if isinstance(route, Blueprint)]
 for route in all_routes:
     app.register_blueprint(route)
 
+
 def app_factory(app, config=None, with_db=False):
+
     current_app = app.config.from_object(config) if config else app
+
+    # db = SQLAlchemy(app=current_app)
+
     return app
