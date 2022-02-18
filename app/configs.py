a13c410ffb9aeb147d8ef0b29a02c48a000a582a
Dimand-Sok <dimand.sok@gmail.com>
2022-02-18 07:32:31 +0700

    config file

--- app/configs.py
+++ app/configs.py
@@ -1,3 +1,4 @@
+import os
 from distutils.debug import DEBUG
 from pickle import TRUE
 from re import T
@@ -11,3 +12,6 @@ class DevConfig(object):
 
 class ProConfig(object):
     PRODUCTION = True
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
